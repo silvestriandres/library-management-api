b5c07fc3a9ad6c7c32cfc5f4651a8da5ba35f14a
silvestriandres <silvestri.andres@gmail.com>
2026-05-18 13:29:57 -0300

    feat: implement repository layer for Book entity

---
+++ LibraryManagementApi.API/Repositories/BookRepository.cs
@@ -0,0 +1,49 @@
+﻿using LibraryManagementApi.API.Data;
+using LibraryManagementApi.API.Models;
+using LibraryManagementApi.API.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibraryManagementApi.API.Repositories;
+
+public class BookRepository : IBookRepository
+{
+    private readonly ApplicationDbContext _context;
+
+    public BookRepository(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<IEnumerable<Book>> GetAllAsync()
+    {
+        return await _context.Books.ToListAsync();
+    }
+
+    public async Task<Book?> GetByIdAsync(int id)
+    {
+        return await _context.Books.FindAsync(id);
+    }
+
+    public async Task<Book> CreateAsync(Book book)
+    {
+        _context.Books.Add(book);
+
+        await _context.SaveChangesAsync();
+
+        return book;
+    }
+
+    public async Task UpdateAsync(Book book)
+    {
+        _context.Books.Update(book);
+
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task DeleteAsync(Book book)
+    {
+        _context.Books.Remove(book);
+
+        await _context.SaveChangesAsync();
+    }
+}
---
+++ LibraryManagementApi.API/Repositories/Interfaces/IBookRepository.cs
@@ -0,0 +1,16 @@
+﻿using LibraryManagementApi.API.Models;
+
+namespace LibraryManagementApi.API.Repositories.Interfaces;
+
+public interface IBookRepository
+{
+    Task<IEnumerable<Book>> GetAllAsync();
+
+    Task<Book?> GetByIdAsync(int id);
+
+    Task<Book> CreateAsync(Book book);
+
+    Task UpdateAsync(Book book);
+
+    Task DeleteAsync(Book book);
+}
